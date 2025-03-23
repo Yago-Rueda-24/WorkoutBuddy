8faac786fca2b99d4d57992c96d6151443ba917c
yago <yago@fedora>
2025-03-23 13:15:51 +0100

    Modificación de las clases

--- src/main/java/servicios/RutinaServicios.java
+++ src/main/java/servicios/RutinaServicios.java
@@ -1,5 +1,13 @@
 package servicios;
 
+import entidades.Ejercicio;
+import java.util.ArrayList;
+import utiles.Database;
+
 public class RutinaServicios {
 
+    public ArrayList<Ejercicio> recuperaEjercicios() {
+        String sql ="SELECT * from rutina";
+        return null;
+    }
 }
