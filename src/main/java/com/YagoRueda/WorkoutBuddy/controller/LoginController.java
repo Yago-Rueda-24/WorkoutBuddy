36b2e921eaffe7ad071c181dd1e264e1c0abefe6
yago-vasco <yago.rc.04@gmail.com>
2025-05-16 15:07:08 +0200

    fix(controller): login controller
    Se ha añadido la etiqueta @validated para especificar que ese controlador contiene validaciones

--- src/main/java/com/YagoRueda/WorkoutBuddy/controller/LoginController.java
+++ src/main/java/com/YagoRueda/WorkoutBuddy/controller/LoginController.java
@@ -4,12 +4,14 @@ package com.YagoRueda.WorkoutBuddy.controller;
 import com.YagoRueda.WorkoutBuddy.Service.UserService;
 import com.YagoRueda.WorkoutBuddy.entity.UserEntity;
 import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/login")
+@Validated
 public class LoginController {
 
     private final UserService service;
