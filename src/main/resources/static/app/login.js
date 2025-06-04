130c2bc41a67d53253916d07006b8d993a80cc75
yago-vasco <yago.rc.04@gmail.com>
2025-06-04 22:13:08 +0200

    feature(frontend): restablecimiento de contraseña
    Se ha creado la lógica y la interfaz para la solicitud de restablecimiento de contraseña

--- src/main/resources/static/app/login.js
+++ src/main/resources/static/app/login.js
@@ -17,14 +17,14 @@ async function checkLoginStatus() {
     if (response.ok) {
       const data = await response.json();
       sessionStorage.setItem("user", username);
-      console.log("Respuesta de éxito:", data.message); 
+      console.log("Respuesta de éxito:", data.message);
       window.location.href = "/mainpage.html";
     } else if (response.status === 401) {
       const errorData = await response.json();
       console.error("Error de inicio de sesión:", errorData.message);
-      document.getElementById("messager-error").innerText = errorData.message; 
+      document.getElementById("messager-error").innerText = errorData.message;
     }
-     else {
+    else {
       console.error("Error en la respuesta del servidor. Código:", response.status);
       document.getElementById("messager-error").innerText = "Error al iniciar sesión";
     }
@@ -34,7 +34,28 @@ async function checkLoginStatus() {
   }
 }
 
-document.getElementById("login-form").addEventListener("submit", function(event) {
+document.getElementById("login-form").addEventListener("submit", function (event) {
   event.preventDefault();
   checkLoginStatus();
+});
+
+document.getElementById("forgotPassword").addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const username = document.getElementById("username").value;
+  if (!username) {
+    alert("Por favor, introduce tu nombre de usuario.");
+    return;
+  }
+  fetch("/login/password/" + username, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    }
+
+  }).then(reponse => {
+    alert(reponse.json().message);
+  })
+
+
 });
