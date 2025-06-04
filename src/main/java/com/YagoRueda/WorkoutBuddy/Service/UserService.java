b7dcccc950593dc6897eb904ed7ed97639a51b6e
yago-vasco <yago.rc.04@gmail.com>
2025-06-05 00:18:18 +0200

    build(backend): creación de la función de restablecimiento de contraseñas
    Se ha creado el flujo que permite reestablecer la contraseña. Se almacena la petición de cambio y se utiliza el servicio de correo para enviar un correo

--- src/main/java/com/YagoRueda/WorkoutBuddy/Service/UserService.java
+++ src/main/java/com/YagoRueda/WorkoutBuddy/Service/UserService.java
@@ -1,14 +1,24 @@
 package com.YagoRueda.WorkoutBuddy.Service;
 
 import com.YagoRueda.WorkoutBuddy.DTO.SignupDTO;
+import com.YagoRueda.WorkoutBuddy.entity.PetPasswordEntity;
 import com.YagoRueda.WorkoutBuddy.entity.UserEntity;
+import com.YagoRueda.WorkoutBuddy.exception.InpuDataException;
+import com.YagoRueda.WorkoutBuddy.repository.PetPasswordRepository;
 import com.YagoRueda.WorkoutBuddy.repository.UserRepository;
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailAuthenticationException;
+import org.springframework.mail.MailSendException;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -16,11 +26,17 @@ public class UserService {
     private final UserRepository repository;
     private MessageDigest hash;
 
-    public UserService(UserRepository repository) {
+    private final PetPasswordRepository petPasswordRepository;
+
+    @Autowired
+    private MailService mailService;
+
+    public UserService(UserRepository repository, PetPasswordRepository petPasswordRepository) {
         this.repository = repository;
-        try{
+        this.petPasswordRepository = petPasswordRepository;
+        try {
             this.hash = MessageDigest.getInstance("SHA-256");
-        }catch(NoSuchAlgorithmException e){
+        } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
 
@@ -41,13 +57,13 @@ public class UserService {
         if (foundUser == null) {
             return false;
         }
-        return foundUser.getPassword().equals(new String(hashed_password,StandardCharsets.UTF_8));
+        return foundUser.getPassword().equals(new String(hashed_password, StandardCharsets.UTF_8));
     }
 
     public int signUp(SignupDTO signup) {
         boolean exists = repository.existsByUsername(signup.getUsername());
         boolean correct_password = signup.getPasswordrepeat().equals(signup.getPassword());
-        if(!correct_password){
+        if (!correct_password) {
             return 1;
         }
         if (exists) {
@@ -63,6 +79,47 @@ public class UserService {
         user.setPassword(new String(hashed_password, StandardCharsets.UTF_8));
         repository.save(user);
         return 0;
+    }
+
+    public void lostPassword(String username) throws InpuDataException, MessagingException {
+        if (username == null || username.trim().isEmpty()) {
+            throw new InpuDataException("El nombre de usuario no puede ser vacio");
+        }
+
+        if (!repository.existsByUsername(username)) {
+            throw new InpuDataException("El usuario que has intrducido no existe");
+        }
+
+        UserEntity user = repository.findByUsername(username);
+        long id = user.getId();
+        if (user.getEmail() == null || user.getEmail().trim().isEmpty()) {
+            throw new InpuDataException("El usuario no tiene un correo asignado");
+        }
+
+
+        List<PetPasswordEntity> openPetitions = petPasswordRepository.findByUserIdAndUtilizadoFalseAndExpiradoFalse(id);
+        if (!openPetitions.isEmpty()) {
+            openPetitions.forEach(arrPet -> arrPet.setExpirado(true));
+            petPasswordRepository.saveAll(openPetitions);
+        }
+
+
+        PetPasswordEntity pet = new PetPasswordEntity();
+        pet.setUser(user);
+        pet.setExpirado(false);
+        pet.setUtilizado(false);
+        String token = UUID.randomUUID().toString();
+        pet.setToken(token);
+
+        petPasswordRepository.save(pet);
+
+        try {
+            mailService.mailLostPassword(user, token);
+        } catch (MailSendException | MailAuthenticationException e) {
+            throw new MessagingException(e.getMessage());
+        }
+
+
     }
 
 
