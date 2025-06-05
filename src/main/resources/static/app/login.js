fd3f0ada141f76b4064add31f0aa84870ec6b61c
yago-vasco <yago.rc.04@gmail.com>
2025-06-05 13:10:34 +0200

    bugfix(frontend): mensaje sobre el envio de correo
    Se ha corregido el problema que provocaba que el mensaje no se moestrara correctamente

--- src/main/resources/static/app/login.js
+++ src/main/resources/static/app/login.js
@@ -52,10 +52,15 @@ document.getElementById("forgotPassword").addEventListener("click", function (ev
     headers: {
       "Content-Type": "application/json",
     }
-
-  }).then(reponse => {
-    alert(reponse.json().message);
   })
+    .then(response => response.json())
+    .then(data => {
+      alert(data.message);
+    })
+    .catch(error => {
+      alert("Error al recuperar la contraseña");
+      console.error(error);
+    });
 
 
 });
