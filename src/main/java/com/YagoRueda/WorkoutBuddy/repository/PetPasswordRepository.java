09f089f2c1406d25e9b84456fd682762c845e706
yago-vasco <yago.rc.04@gmail.com>
2025-06-06 13:22:58 +0200

    build(repository): Repositorio de peticiones de cambio de password

--- src/main/java/com/YagoRueda/WorkoutBuddy/repository/PetPasswordRepository.java
+++ src/main/java/com/YagoRueda/WorkoutBuddy/repository/PetPasswordRepository.java
@@ -3,12 +3,18 @@ package com.YagoRueda.WorkoutBuddy.repository;
 import com.YagoRueda.WorkoutBuddy.entity.PetPasswordEntity;
 import com.YagoRueda.WorkoutBuddy.entity.RoutineEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PetPasswordRepository extends JpaRepository<PetPasswordEntity, Long> {
 
     List<PetPasswordEntity> findByUserIdAndUtilizadoFalseAndExpiradoFalse(Long userId);
+
+    @Query("SELECT p FROM PetPasswordEntity p WHERE p.user.username = :username AND p.token = :token")
+    Optional<PetPasswordEntity> findByUsernameAndToken(@Param("username") String username, @Param("token") String token);
 
 
 }
