ded2072c10580b5f3d128a28d64710f3e24e5a58
yago-vasco <yago.rc.04@gmail.com>
2025-06-06 13:22:29 +0200

    build(Exception): Excepción que representa una petición invalida

---
+++ src/main/java/com/YagoRueda/WorkoutBuddy/exception/InvalidaPetitionException.java
@@ -0,0 +1,7 @@
+package com.YagoRueda.WorkoutBuddy.exception;
+
+public class InvalidaPetitionException extends RuntimeException {
+    public InvalidaPetitionException(String message) {
+        super(message);
+    }
+}
