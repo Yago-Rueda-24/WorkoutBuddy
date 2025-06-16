6c4944dc74e57423247fdf8ac8d3c909f7691788
yago-vasco <yago.rc.04@gmail.com>
2025-06-16 21:53:29 +0200

    feature(userrepo): query para obtención de usuarios
    Se han implementado una query de recuperación de usuarios y otra de recuperación de usuarios filtrano por nombre

--- src/main/java/com/YagoRueda/WorkoutBuddy/repository/UserRepository.java
+++ src/main/java/com/YagoRueda/WorkoutBuddy/repository/UserRepository.java
@@ -1,11 +1,26 @@
 package com.YagoRueda.WorkoutBuddy.repository;
 
 import com.YagoRueda.WorkoutBuddy.entity.UserEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+
 
     UserEntity findByUsername(String username);
 
     boolean existsByUsername(String username);
+
+    List<UserEntity> findByUsernameStartingWith(String prefix, Pageable pageable);
+
+
+    @Query(value = "SELECT * FROM user_entity LIMIT :limit", nativeQuery = true)
+    List<UserEntity> findLimitedUsers(@Param("limit") int limit);
+
+
 }
