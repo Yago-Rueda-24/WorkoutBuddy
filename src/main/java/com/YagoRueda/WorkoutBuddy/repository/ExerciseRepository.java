2322f283ec9f122fa12efc47f7a2440955338f4b
yago-vasco <yago.rc.04@gmail.com>
2025-06-20 16:07:54 +0200

    docs(exerciseRepository): documentación del repositorio de ejercicios

--- src/main/java/com/YagoRueda/WorkoutBuddy/repository/ExerciseRepository.java
+++ src/main/java/com/YagoRueda/WorkoutBuddy/repository/ExerciseRepository.java
@@ -7,10 +7,26 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ExerciseRepository extends JpaRepository<ExerciseEntity,Long> {
+    /**
+     * Encuentra un ejercicio en base a su id
+     * @param id ID del ejercicio
+     * @return Entidad de BD que representa al ejercicio
+     */
     ExerciseEntity findById(long id);
 
+
+    /**
+     * Encuentra todos los ejercicios de una rutina en base al nombre de la rutina
+     * @param name nombre de la rutina
+     * @return lista de ejercicios de la rutina
+     */
     List<ExerciseEntity> findByRoutineName(String name);
 
 
+    /**
+     * Encuentra un ejercicio en base a su nombre
+     * @param name nombre del ejercicio
+     * @return Entidad de BD que representa al ejercicio
+     */
     ExerciseEntity findByName(String name);
 }
