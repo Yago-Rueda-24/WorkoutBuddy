db7a034ba688e9284fad30b5bee45e589d303e01
yago-vasco <yago.rc.04@gmail.com>
2025-06-20 16:14:13 +0200

    docs(petitionRepository): documentación del repositorio de peticiones de reestablecimiento de contraseña

--- src/main/java/com/YagoRueda/WorkoutBuddy/repository/PetPasswordRepository.java
+++ src/main/java/com/YagoRueda/WorkoutBuddy/repository/PetPasswordRepository.java
@@ -11,8 +11,19 @@ import java.util.Optional;
 
 public interface PetPasswordRepository extends JpaRepository<PetPasswordEntity, Long> {
 
+    /**
+     * Busca las peticiones de un usuario que no hayan expirado y no se hayan usado
+     * @param userId ID del usuario
+     * @return Lista de peticiones
+     */
     List<PetPasswordEntity> findByUserIdAndUtilizadoFalseAndExpiradoFalse(Long userId);
 
+    /**
+     * Busca una petición en base al usario que la hizo y al token de la petición
+     * @param username Nombre de usuario
+     * @param token Token de la petición
+     * @return Entidad de la petición, si existe
+     */
     @Query("SELECT p FROM PetPasswordEntity p WHERE p.user.username = :username AND p.token = :token")
     Optional<PetPasswordEntity> findByUsernameAndToken(@Param("username") String username, @Param("token") String token);
 
