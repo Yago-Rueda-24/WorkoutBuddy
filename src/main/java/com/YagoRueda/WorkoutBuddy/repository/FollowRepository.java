5b639dbe5abbb03fc46d78a1a91181947d36de16
yago-vasco <yago.rc.04@gmail.com>
2025-06-29 16:28:43 +0200

    docs(follow repository): Documentación de repositorio de follows

--- src/main/java/com/YagoRueda/WorkoutBuddy/repository/FollowRepository.java
+++ src/main/java/com/YagoRueda/WorkoutBuddy/repository/FollowRepository.java
@@ -6,12 +6,34 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FollowRepository extends JpaRepository<FollowEntity,Long> {
 
+    /**
+     * Indica si existe follow indicando el seguido y el seguidor
+     * @param follower {@link UserEntity} del usuario seguidor
+     * @param followed {@link UserEntity} del usuario seguido
+     * @return  {@code true} si existe {@code false} si no existe
+     */
     boolean existsByFollowerAndFollowed(UserEntity follower, UserEntity followed);
 
+    /**
+     * Devuelve el seguimiento entre 2 usuario, si existe
+     * @param follower {@link UserEntity} del usuario seguidor
+     * @param followed {@link UserEntity} del usuario seguido
+     * @return {@link FollowEntity} del seguimiento si existe, si no devuelve {@code null}
+     */
     FollowEntity findByFollowerAndFollowed(UserEntity follower, UserEntity followed);
 
+    /**
+     * Devuelve el número de usuarios que están siguiendo al usuario dado
+     * @param followed {@link UserEntity} del usuario del que se quiere saber la información
+     * @return {@code long} con el número seguidores del usuario
+     */
     long countByFollowed(UserEntity followed);
 
+    /**
+     * Devuelve cuántos usuarios está siguiendo el usuario dado.
+     * @param follower {@link UserEntity} del usuario del que se quiere saber la información
+     * @return {@code long} con el número seguidos del usuario
+     */
     long countByFollower(UserEntity follower);
 
 
